7495345c742792d8b76af561a9e4b99d7db6f684
Наима <113019779+Naimabilal19@users.noreply.github.com>
2023-03-08 16:45:07 +0200

    Add files via upload

---
+++ ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int[] arr1 = { 2, 76, 6, 88, 10 , 54};
+            int[] arr2 = { 1, 4, 6, 86, 9, 54 };
+
+            var res = arr1.Except(arr2);
+            foreach (var s in res)
+                Console.WriteLine(s);
+            Console.WriteLine('\n');
+
+            //2
+            res = arr1.Intersect(arr2);
+
+            foreach (var s in res)
+                Console.WriteLine(s);
+            Console.WriteLine('\n');
+
+            //3
+            res = arr1.Union(arr2);
+
+            foreach (var s in res)
+                Console.WriteLine(s);
+            Console.WriteLine('\n');
+
+            //4
+            res = res.Distinct();
+            foreach (var s in res)
+                Console.WriteLine(s);
+            Console.WriteLine('\n');
+        }
+    }
+}
